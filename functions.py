a037d88bcafca5381ddb5f5c61ce0b624a4ef7c2
yilmaz-a <arnaud.yilmaz@efrei.net>
2024-04-17 18:25:39 +0200

    NORD OUEST FONCTIONELLE

--- functions.py
+++ functions.py
@@ -13,21 +13,27 @@ def afficher_matrice_couts(tableau):
     matrice_couts = [ligne[:-1] for ligne in lignes_intermediaires]
     for ligne in matrice_couts:
         print(ligne)
+    return matrice_couts
 
 #Proposition de transport (Nord-Ouest)
 #1.Extraction des offres
 def afficher_offre(tableau):
-    lignes_intermediaires = tableau[:1]
+    lignes_intermediaires = tableau[1:-1]
     offre = [ligne[-1] for ligne in lignes_intermediaires]
     for ligne in offre:
         print(ligne)
+    return offre
 #2.Extraction des demandes
 def afficher_demande(tableau):
     demande = tableau[-1]
     print(demande)
-
+    return demande
 #NORD OUEST
 def coin_nord_ouest(offres, demandes, couts):
+    # Copie les listes pour éviter de modifier les originales
+    offres = offres[:]
+    demandes = demandes[:]
+
     # Initialiser la solution de transport avec des zéros
     solution = [[0] * len(demandes) for _ in range(len(offres))]
     i, j = 0, 0  # indices pour parcourir les lignes et les colonnes
@@ -40,24 +46,10 @@ def coin_nord_ouest(offres, demandes, couts):
         offres[i] -= quantite
         demandes[j] -= quantite
 
-        # Avancer dans la matrice
-        if offres[i] == 0 and i < len(offres) - 1:
+        # Avancer dans la matrice correctement
+        if offres[i] == 0:
             i += 1
-        elif demandes[j] == 0 and j < len(demandes) - 1:
+        if demandes[j] == 0:
             j += 1
 
     return solution
-
-
-# Exemple d'utilisation
-offres = [20, 30, 25]  # Quantités disponibles dans chaque source
-demandes = [25, 25, 25]  # Quantités requises par chaque destination
-couts = [
-    [8, 6, 10],
-    [9, 7, 4],
-    [3, 4, 2]
-]  # Matrice des coûts de transport
-
-solution = coin_nord_ouest(offres, demandes, couts)
-for ligne in solution:
-    print(ligne)
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from functions import *
-tableau = read_file("tab1.txt")
+tableau = read_file("tableau/table1.txt")
 print(tableau)
 afficher_demande(tableau)
 afficher_matrice_couts(tableau)
+print("Bienvenue dans le système de sélection de transport !")
+print("Voici les options disponibles :")
+print("1. NORD OUEST - Le trajet rapide vers le nord-ouest.")
+print("2. Ballas Hammer - L'expérience ultime de confort et de luxe.")
+# Boucle jusqu'à ce que l'utilisateur entre une option valide
+while True:
+    choix = input("Veuillez entrer votre choix (1 ou 2) : ")
+    if choix in ['1', '2']:
+        print(f"Vous avez choisi l'option {choix}. Merci !")
+        if choix == '1':
+            couts = afficher_matrice_couts(tableau)
+            offres = afficher_offre(tableau)
+            demandes = afficher_demande(tableau)
+            print("Offres", offres, "Demandes", demandes)
+            print(coin_nord_ouest(offres, demandes, couts))
+    else:
+        print("Entrée invalide. Veuillez choisir entre 1 et 2.")
