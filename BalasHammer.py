5fb65f4a8eed42fe5c1d442fa5da24c0066d1525
raphber94 <99801418+raphber94@users.noreply.github.com>
2024-05-05 19:35:13 +0200

    Ballas Hammer fini

--- BalasHammer.py
+++ BalasHammer.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 from tabulate import tabulate
 import random
@@ -5,9 +7,9 @@ import random
 
 class BalasHammer:
     def __init__(self, tableau):
-            self.penalties = None
-            self.tableau = tableau
-            self.solution = np.zeros_like(self.tableau.couts)
+        self.penalties = None
+        self.tableau = tableau
+        self.solution = np.full((tableau.couts.shape[0],tableau.couts.shape[1]), fill_value=None)
 
     def display_matrix_tabulate(self, matrix, title="Matrix"):
         print(title + ":\n" + tabulate(matrix, tablefmt="fancy_grid", headers="keys", showindex="always"))
@@ -16,90 +18,115 @@ class BalasHammer:
         print(title + " as list:")
         print(matrix.tolist())
 
-    def minimum_cost_method(self):
-        # Crée des copies des tableaux d'offre et de demande
-        offres_copy = np.copy(self.tableau.offres)
-        demandes_copy = np.copy(self.tableau.demandes)
+    def calculate_penalties(self,cost_copy):
+        print("Cost copy de la fonction")
+        print(cost_copy)
+        penalties = {'row': [], 'column': []}
+        if np.all(np.isinf(cost_copy)):
+            return None
+        else:
+            for i in range(cost_copy.shape[0]):
+                row = cost_copy[i, :]
+                sorted_row = np.sort(row[row != np.inf])  # trier les coûts qui ne sont pas infinis
+                print("Sorted Row")
+                print(sorted_row)
+                if len(sorted_row) >= 2:
+                    penalty = sorted_row[1] - sorted_row[0]  # calculer la différence entre les deux plus petits coûts
+                    penalties['row'].append((penalty, i))
+                elif len(sorted_row)==1:
+                    penalty= sorted_row[0]
+                    penalties['row'].append((penalty, i))
 
-        # Continue jusqu'à ce que l'offre et la demande soient complètement épuisées
-        while np.any(offres_copy > 0) and np.any(demandes_copy > 0):
-            # Trouve la cellule avec le coût le plus bas
-            min_cost = np.min(self.tableau.couts)
-            min_cost_indices = np.where(self.tableau.couts == min_cost)
-
-            for index in zip(min_cost_indices[0], min_cost_indices[1]):
-                i, j = index
-                # Allocation basée sur le minimum de l'offre ou de la demande restante
-                quantity = min(offres_copy[i], demandes_copy[j])
-                self.solution[i, j] += quantity
-                offres_copy[i] -= quantity
-                demandes_copy[j] -= quantity
-
-                # Si l'offre ou la demande de cette cellule est épuisée, mettez son coût à l'infini
-                if offres_copy[i] == 0 or demandes_copy[j] == 0:
-                    self.tableau.couts[i, j] = np.inf
-
-    def calculate_penalties(self):
-        self.penalties = {'row': [], 'column': []}
-        for i in range(self.tableau.couts.shape[0]):
-            row = self.tableau.couts[i, :]
-            sorted_row = np.sort(row[row != np.inf])  # trier les coûts qui ne sont pas infinis
-            if len(sorted_row) >= 2:
-                penalty = sorted_row[1] - sorted_row[0]  # calculer la différence entre les deux plus petits coûts
-                self.penalties['row'].append((penalty, i))
-
-        for j in range(self.tableau.couts.shape[1]):
-            column = self.tableau.couts[:, j]
-            sorted_column = np.sort(column[column != np.inf])  # trier les coûts qui ne sont pas infinis
-            if len(sorted_column) >= 2:
-                penalty = sorted_column[1] - sorted_column[0]  # calculer la différence entre les deux plus petits coûts
-                self.penalties['column'].append((penalty, j))
+            for j in range(cost_copy.shape[1]):
+                column = cost_copy[:, j]
+                sorted_column = np.sort(column[column != np.inf])  # trier les coûts qui ne sont pas infinis
+                print("Sorted column")
+                print(sorted_column)
+                if (len(sorted_column)) != 0:
+                    if len(sorted_column) >= 2:
+                        penalty = sorted_column[1] - sorted_column[0]  # calculer la différence entre les deux plus petits coûts
+                        penalties['column'].append((penalty, j))
+                    elif len(sorted_column)==1:
+                        penalty= sorted_column[0]
+                        penalties['column'].append((penalty, j))
+        return penalties
 
     def select_and_adjust(self):
-        supply = np.copy(self.tableau.offres)
-        demand = np.copy(self.tableau.demandes)
-        rows_processed = np.zeros_like(supply, dtype=bool)
-        cols_processed = np.zeros_like(demand, dtype=bool)
-
-        iteration = 0
-        while np.any(supply > 0) and np.any(demand > 0):
-            iteration += 1
-            print(f"\nIteration {iteration}:")
-
-            self.calculate_penalties()
-
-            max_penalty = max(self.penalties['column'] + self.penalties['row'], key=lambda x: x[0])
-            max_penalty_indices = [index for index in self.penalties['column'] + self.penalties['row'] if
-                                   index[0] == max_penalty[0]]
-
-            min_cost = np.inf
-            min_cost_index = None
-            for index in max_penalty_indices:
-                if len(index) >= 3:
-                    i, j = index[1], index[2]
-                    if self.tableau.couts[i, j] < min_cost and not rows_processed[i] and not cols_processed[j]:
-                        min_cost = self.tableau.couts[i, j]
-                        min_cost_index = (i, j)
-
-            if min_cost_index is None:
+        offer_copy = copy.deepcopy(self.tableau.offres)
+        demand_copy = copy.deepcopy(self.tableau.demandes)
+        cost_copy = copy.deepcopy(self.tableau.couts.astype(float))
+        while True:
+            penalities = self.calculate_penalties(cost_copy)
+            if(penalities==None):
                 break
+            row_index=None
+            column_index=None
+            max_penalty = -1
+            for i in penalities['row']:
+                if i[0]>max_penalty:
+                    max_penalty=i[0]
+                    row_index=i[1]
 
-            i, j = min_cost_index
-            flow = min(supply[i], demand[j])
-            print(f"Choix de la cellule ({i + 1}, {j + 1}) avec un coût de {min_cost} et un flux de {flow}")
+            for i in penalities['column']:
+                if i[0]>max_penalty:
+                    max_penalty=i[0]
+                    column_index=i[1]
+                    row_index=None
 
-            self.solution[i, j] += flow
+            minimal_cost=np.inf
+            if column_index==None:
+                for i in range(len(cost_copy[row_index])):
+                    if cost_copy[row_index][i]<minimal_cost:
+                        minimal_cost=cost_copy[row_index][i]
+                        column_index=i
+            else:
+                for i in range(len(cost_copy)):
+                    if cost_copy[i,column_index]<minimal_cost:
+                        minimal_cost=cost_copy[i,column_index]
+                        row_index = i
 
-            # Mark the row and column as processed
-            rows_processed[i] = True
-            cols_processed[j] = True
+            quantity_max = min(offer_copy[row_index],demand_copy[column_index])
+            self.solution[row_index][column_index] = quantity_max
+            #Si la quantité max est détectée parmi les offres / donc on va procéder le long de la ligne
+            if(offer_copy[row_index]==quantity_max):
+                #On va mettre une quantité qui correspond à l'offre max, donc l'offre dispo passe à 0
+                offer_copy[row_index]=0
+                # On enlève la quantité qu'on ajoute dans le tableau à la demande sur la colonne correspondante
+                demand_copy[column_index]= demand_copy[column_index]-quantity_max
 
-            # If the supply or demand for this cell is exhausted, set its cost to infinity
-            if supply[i] == 0 or demand[j] == 0:
-                self.tableau.couts[i, j] = np.inf
+                #On a maximisé la quantité par rapport à l'offre, donc le reste de la ligne passe à 0 (si jamais modifiée)
+                for i in range(len(self.solution[0])):
+                    if self.solution[row_index][i]==None:
+                        self.solution[row_index][i]=0
+                    cost_copy[row_index][i]=np.inf
 
-            print("Solution actuelle:")
+                #Dans le cas où l'offre et la demande sont toutes les deux maximisées, on remplis aussi la colonne de 0
+                if demand_copy[column_index]==0:
+                    for i in range(len(self.solution)):
+                        if self.solution[i][column_index]==None:
+                            self.solution[i][column_index]=0
+                        cost_copy[i][column_index] = np.inf
+
+            elif(demand_copy[column_index]==quantity_max):
+                demand_copy[column_index] = 0
+                offer_copy[row_index] = offer_copy[row_index] - quantity_max
+                for i in range(len(self.solution)):
+                    if self.solution[i][column_index] == None:
+                        self.solution[i][column_index] = 0
+                    cost_copy[i][column_index] = np.inf
+
+
+
+            print("Cost copie")
+            print(cost_copy)
+            print("Solution actuelle")
             print(self.solution)
+        print("Solution actuelle")
+        print(self.solution)
+
+
+
+
 
     def find_initial_solution(self):
         # Implémentation de l'algorithme Balas-Hammer pour trouver la solution initiale
@@ -118,7 +145,7 @@ class BalasHammer:
                 if supply[i] == 0 or demand[j] == 0:
                     continue
                 flow = min(supply[i], demand[j])
-                self.solution[i, j] = flow
+                self.solution[i][j] = flow
                 supply[i] -= flow
                 demand[j] -= flow
 
@@ -132,14 +159,12 @@ class BalasHammer:
             print(f"Colonne {penalty[1] + 1}: {penalty[0]}")
 
     def execute(self):
-        self.find_initial_solution()
         self.select_and_adjust()
         # Vérification que toute l'offre et la demande ont été complètement utilisées
         if not np.all(self.tableau.offres == 0) or not np.all(self.tableau.demandes == 0):
             print("Erreur: Toute l'offre ou la demande n'a pas été complètement utilisée.")
         print("Solution finale:")
         self.display_matrix_tabulate(self.solution, "Solution Balas-Hammer")
-        self.display_matrix_brackets(self.solution, "Solution Balas-Hammer Brackets")
         total_cost = np.sum(self.solution * self.tableau.couts)
         print("Coût total de la solution:", total_cost)
         self.tableau.check_supply_demand()
